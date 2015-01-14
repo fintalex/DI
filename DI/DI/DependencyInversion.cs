ede353ef87a8cb3340333635d5870e3bdee3bf3d
fintalex <fintalex@mail>
2015-01-14 21:33:23 +0300

    DependencyInversion

---
+++ DI/DI/DependencyInversion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    // класс Salutation зависит от интерфейса IMessageWriter , 
+    // и поэтому запрашивает экземпляр реализующий этот интерфейс в свой конструктор
+    public class Salutation
+    {
+        private readonly IMessageWriter writer;
+        // внедрение зависимости через конструктор (Constructor Injection)
+        public Salutation(IMessageWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void Exclaim(string mess)
+        {
+            // использование зависимости - нашего переданного экземпляра класса
+            writer.Write(mess);
+        }
+    }
+
+    public interface IMessageWriter
+    {
+        void Write(string message);
+    }
+
+    // реализует интерфейс IMessageWriter путем создания обертки метод Write для класса Console
+    // такая реализация называется патерном "Адаптер"
+    public class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string mess)
+        {
+            Console.WriteLine("Here our message:" + mess);
+        }
+    }
+}
--- DI/DI/Program.cs
+++ DI/DI/Program.cs
@@ -4,55 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DI
+using DependencyInversion;
+
+namespace SOLID
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			// создаем  новый экземпляр класса ConsoleMessageWriter, который мы знаем реализует функциональность вывода на экран
-			// экземпляр мы передает классу Salutation, который теперь знает куда выводить сообщение 
-			IMessageWriter writer = new ConsoleMessageWriter();
+            //// ===========================================  DependencyInversion   =============================================================
+            //// создаем  новый экземпляр класса ConsoleMessageWriter, который мы знаем реализует функциональность вывода на экран
+            //// экземпляр мы передает классу Salutation, который теперь знает куда выводить сообщение 
+            //IMessageWriter writer = new ConsoleMessageWriter();
 
-			//var typeName = .AppSerrings["messageWriter"];
-			Salutation salutation = new Salutation(writer);
-			salutation.Exclaim(" hello DI");
+            ////var typeName = .AppSerrings["messageWriter"];
+            //Salutation salutation = new Salutation(writer);
+            //salutation.Exclaim(" hello DI");
 
-			Console.ReadLine();
+            //Console.ReadLine();
+
+            // ===========================================  DependencyInversion   =============================================================
 		}
 	}
 
-	// класс Salutation зависит от интерфейса IMessageWriter , 
-	// и поэтому запрашивает экземпляр реализующий этот интерфейс в свой конструктор
-	public class Salutation 
-	{
-		private readonly IMessageWriter writer;
-		// внедрение зависимости через конструктор (Constructor Injection)
-		public Salutation(IMessageWriter writer)
-		{
-			this.writer = writer;
-		}
-
-		public void Exclaim(string mess)
-		{
-			// использование зависимости - нашего переданного экземпляра класса
-			writer.Write(mess);
-		}
-	}
-
-	public interface IMessageWriter
-	{
-		void Write(string message);
-	}
-
-	// реализует интерфейс IMessageWriter путем создания обертки метод Write для класса Console
-	// такая реализация называется патерном "Адаптер"
-	public class ConsoleMessageWriter : IMessageWriter
-	{
-		public void Write(string mess)
-		{
-			Console.WriteLine("Here our message:" + mess);
-		}
-	}
+	
 
 }
