4b955e9352aa1e8c5fdb34712649d749876447ea
fintalex <fintalex@mail.ru>
2015-01-16 10:57:56 +0300

    GoodDisignMustBeSOLID2

--- DI/DI/I/GoodDisignMustBeSOLID.cs
+++ DI/DI/I/GoodDisignMustBeSOLID.cs
@@ -44,3 +44,47 @@ namespace GoodDisignMustBeSOLID
 	}
 }
 
+// решение должно быть в проектировании грамотной иерархии интерфейсов
+
+namespace GoodDisignMustBeSOLID2
+{
+	abstract class ServiceClient
+	{
+		public string ServiceUri { set; get; }
+		// оставили здесь эту сигнатуру метода, так как она используется в обоих классах
+		public abstract void SendData(object data);
+	}
+	// создаем наследника нашего интерфейса и задаем в нем второй метода который используется не во всех классах
+	// а подчиненном абстрактном классе не обязательно переопределять родительский абстрактный метод
+	abstract class BufferingServiceClient : ServiceClient
+	{
+		public abstract void Flush();
+	}
+
+	// получается сейчас мы можем создавать классы наследники в которых нужно будет переопределить 
+	// BufferingServiceClient: Flush и SendData
+	// ServiceClient: только SendData
+
+	class HttpServiceClient : ServiceClient
+	{
+		// нам второй метод Flush не нужен был и мы от него избавились
+		public override void SendData(object data)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class BufferingHttpServiceClient : BufferingServiceClient
+	{
+		// а вот другой подчиненный абстрактный класс имеет более расширенный набор методов который мы должны реализовать
+		public override void Flush()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SendData(object data)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
