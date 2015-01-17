009b5d4810cc9592cc74381f9737566d879f385c
fintalex <fintalex@mail>
2015-01-17 23:41:46 +0300

    Reporter

---
+++ DI/DI/D/StrongCoupling.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrongCoupling
+{
+    public interface IReportBuilder
+    {
+        IList<Report> CreateReports();
+    }
+    public interface IReportSender
+    {
+        void Send(Report report);
+    }
+
+    // вместо того чтобы напрямую создавать а потом вызывать методы экземпляров класса
+    // мы просто передаем Их ( обязательно они должны будут реализовывыть интерфейс) в конструктор Reporter
+    public class Reporter
+    {
+        private readonly IReportBuilder reportbuilder;
+        private readonly IReportSender reportSender;
+        public Reporter (IReportBuilder reportBuilder, IReportSender reportSender)
+        {
+            this.reportbuilder = reportbuilder;
+            this.reportSender = reportSender;
+        }
+        public void SendReports()
+        {
+            IList<Report> reports = reportbuilder.CreateReports();
+            if (reports.Count == 0)
+                throw new Exception();
+            foreach (Report report in reports)
+            {
+                reportSender.Send(report);
+            }
+        }
+    }
+
+    public class Report
+    {
+        // some fields of reports
+    }
+
+    // теперь если мы хотим отпаравлять отчеты через Sms - 
+    // надо просто создать класс реализующий интерфейс - и передавать его в конструктор Reporter
+}
