faf4977625a808bea88e75171ad7eaae5c667562
fintalex <fintalex@mail.ru>
2015-02-09 11:19:32 +0300

    Copy project from Martins book

---
+++ DI/AgilePrinciplesPatterns/Copy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilePrinciplesPatterns
+{
+	public class Copier
+	{
+		public static void Copy()
+		{
+			int c;
+			while ((c = Keyboard.Read()) != -1)
+				Print.Write(c);
+		}
+	}
+
+	public static class Print
+	{
+		public static void Write(int c)
+		{
+			// типа печатаем
+		}
+	}
+	public static class Keyboard
+	{
+		public static int Read()
+		{
+			int c = 0;
+			return c;
+		}
+	}
+}
