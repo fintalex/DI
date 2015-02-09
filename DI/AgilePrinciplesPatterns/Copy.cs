a8cf43345862f2da2449700f786b3f5f2ca4325b
fintalex <fintalex@mail.ru>
2015-02-09 12:00:25 +0300

    Copy
    Добавили новых пожеланий заказчика - код испортился

--- DI/AgilePrinciplesPatterns/Copy.cs
+++ DI/AgilePrinciplesPatterns/Copy.cs
@@ -4,15 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// пример плохой программы
 namespace AgilePrinciplesPatterns
 {
+	 // программу, которая выводит непосредственно на принтер текст, вводимый с клавиатуры
+	//public class Copier
+	//{
+	//	public static void Copy()
+	//	{
+	//		int c;
+	//		while ((c = Keyboard.Read()) != -1)
+	//			Print.Write(c);
+	//	}
+	//}
+	//======= если надо добавить условие, что программа должна читать с перфоленты=====
+	//public class Copier
+	//{
+	//	// не забудьте сбросить этот флаг
+	//	public static bool ptFlag = false;
+	//	public static void Copy()
+	//	{	
+	//		int c;
+	//		while ((c = (ptFlag? PaperTape.Read() :  Keyboard.Read())) != -1)
+	//			Print.Write(c);
+	//	}
+	//}
+	//========= но если надо использовать Copy для выводана перфоленту ===================
 	public class Copier
 	{
+		// не забудьте сбросить этот флаги
+		public static bool ptFlag = false;
+		public static bool punchFlag = false;
+
 		public static void Copy()
 		{
 			int c;
-			while ((c = Keyboard.Read()) != -1)
-				Print.Write(c);
+			while ((c = (ptFlag ? PaperTape.Read() : Keyboard.Read())) != -1)
+			{
+				if (punchFlag)
+					PaperTape.Punch(c);
+				else
+					Print.Write(c);
+			}
 		}
 	}
 
@@ -29,6 +62,18 @@ namespace AgilePrinciplesPatterns
 		{
 			int c = 0;
 			return c;
+		}
+	}
+	public static class PaperTape
+	{
+		public static int Read()
+		{
+			int c = 0;
+			return c;
+		}
+		public static void Punch(int c)
+		{
+			// типа печатаем
 		}
 	}
 }
