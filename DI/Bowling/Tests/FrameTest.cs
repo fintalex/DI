599ebfa0fae0a465b2b6fecaede1690b6ad66ea8
fintalex <fintalex@mail.ru>
2015-02-10 10:49:07 +0300

    Боулинг
    из книги мартина
    начало

---
+++ DI/Bowling/Tests/FrameTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Bowling;
+
+[TestFixture]
+public class FrameTest
+{
+	[Test]
+	public void TestScoreNoThrows()
+	{
+		Frame f = new Frame();
+		Assert.AreEqual(0, f.Score);
+	}
+	 
+}
+
---
+++ DI/Bowling/Tests/ThrowTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+public class ThrowTest
+{
+	[Test]
+	public void Test
+	{ 
+
+	}
+}
+
