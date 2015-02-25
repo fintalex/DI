94efe73b5efa9d314291f555331330459a6500aa
fintalex <fintalex@mail>
2015-02-25 23:23:44 +0300

    AsynchronousLogger

---
+++ DI/AsynchronousLogger/AsynchronousLogger.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Threading;
+namespace AsynchronousLogger
+{
+    public interface Logger
+    {
+        void LogMessage(String msg);
+    }
+    public class AsynchronousLogger : Logger
+    {
+        private ArrayList messages =
+        ArrayList.Synchronized(new ArrayList());
+        private Thread t;
+        private bool running;
+        private int logged;
+        private TextWriter logStream;
+        public AsynchronousLogger(TextWriter stream)
+        {
+            logStream = stream;
+            running = true;
+            t = new Thread(new ThreadStart(MainLoggerLoop));
+            t.Priority = ThreadPriority.Lowest;
+            t.Start();
+        }
+        private void MainLoggerLoop()
+        {
+            while (running)
+            {
+                LogQueuedMessages();
+                SleepTillMoreMessagesQueued();
+                Thread.Sleep(10); // Напомните, чтобы я объяснил это место.
+            }
+        }
+        private void LogQueuedMessages()
+        {
+            while (MessagesInQueue() > 0)
+                LogOneMessage();
+        }
+        private void LogOneMessage()
+        {
+            string msg = (string)messages[0];
+            messages.RemoveAt(0);
+            logStream.WriteLine(msg);
+            logged++;
+        }
+        private void SleepTillMoreMessagesQueued()
+        {
+            lock (messages)
+            {
+                Monitor.Wait(messages);
+            }
+        }
+        public void LogMessage(String msg)
+        {
+            messages.Add(msg);
+            WakeLoggerThread();
+        }
+        public int MessagesInQueue()
+        {
+            return messages.Count;
+        }
+        public int MessagesLogged()
+        {
+            return logged;
+        }
+        public void Stop()
+        {
+            running = false;
+            WakeLoggerThread();
+            t.Join();
+        }
+        private void WakeLoggerThread()
+        {
+            lock (messages)
+            {
+                Monitor.PulseAll(messages);
+            }
+        }
+    }
+}
---
+++ DI/AsynchronousLogger/AsynchronousLoggerTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+namespace AsynchronousLogger
+{
+    [TestFixture]
+    public class TestLog
+    {
+        private AsynchronousLogger logger;
+        private int messagesLogged;
+        [SetUp]
+        protected void SetUp()
+        {
+            messagesLogged = 0;
+            logger = new AsynchronousLogger(Console.Out);
+            Pause();
+        }
+        [TearDown]
+        protected void TearDown()
+        {
+            logger.Stop();
+        }
+        [Test]
+        public void OneMessage()
+        {
+            logger.LogMessage("одно сообщение");
+            CheckMessagesFlowToLog(1);
+        }
+        [Test]
+        public void TwoConsecutiveMessages()
+        {
+            logger.LogMessage("другое");
+            logger.LogMessage("и еще одно");
+            CheckMessagesFlowToLog(2);
+        }
+        [Test]
+        public void ManyMessages()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                logger.LogMessage(string.Format("сообщение:{0}", i));
+                CheckMessagesFlowToLog(1);
+            }
+        }
+        private void CheckMessagesFlowToLog(int queued)
+        {
+            CheckQueuedAndLogged(queued, messagesLogged);
+            Pause();
+            messagesLogged += queued;
+            CheckQueuedAndLogged(0, messagesLogged);
+        }
+        private void CheckQueuedAndLogged(int queued, int logged)
+        {
+            Assert.AreEqual(queued, logger.MessagesInQueue(), "в очереди");
+            Assert.AreEqual(logged, logger.MessagesLogged(), "зарегистрировано");
+        }
+        private void Pause()
+        {
+            Thread.Sleep(50);
+        }
+    }
+}
